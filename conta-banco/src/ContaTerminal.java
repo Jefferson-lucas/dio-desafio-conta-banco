16095592666b42d06b5fa2fff1609eacf85085c0
Jefferson-lucas <jefferson.lucascsilva@gmail.com>
2024-08-19 12:43:02 -0300

    fix: Final version

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -17,20 +17,19 @@ public class ContaTerminal {
         System.out.println("Digite o numero da conta: ");
         numeroConta = scanner.nextInt();
 
-        System.out.println("Digite seu nome: ");
-        nomeCliente = scanner.nextLine();
-
         //Esvaziando o buffer do scanner para não dar erro quando nome contiver espaços
         scanner.nextLine();
 
-        System.out.println("Digite o saldo disponivel: ");
+        System.out.println("Digite seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo disponivel (Informar centavos separados por ,): ");
         scanner.reset();
         saldo = scanner.nextDouble();
         
         scanner.close();
 
         System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta " + 
-                            numeroConta + " e o seu saldo " + saldo + " já está disponível para saque");
-        
+                            numeroConta + " e o seu saldo de " + saldo + " já está disponível para saque."); 
     }
 }
